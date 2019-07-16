43db012bdcd145d15b005a3ffe7c190e2634a80e
andresberrios <andres.berrios.j@gmail.com>
2019-07-16 20:19:01 +0200

    Remove eosjs-legacy and imrpove multi test

--- packages/multi/__tests__/multi.spec.ts
+++ packages/multi/__tests__/multi.spec.ts
@@ -12,8 +12,6 @@ import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig'
 import fetch from 'node-fetch'
 import { TextDecoder, TextEncoder } from 'util'
 
-import * as Eos from 'eosjs-legacy'
-
 import { Morpheos } from '@tokenwrap/core-eosio'
 import { DGoods } from '@tokenwrap/dgoods'
 import { SimpleAssets } from '@tokenwrap/simpleassets'
@@ -21,8 +19,6 @@ import { MultiWrapper } from '../src/index'
 
 const httpEndpoint = 'https://api.jungle.alohaeos.com'
 const privateKey = '5K3MYohjJLNfNGD6Dg2xuqiZcgKejos9bLHwwjkyw7eH3JxvyZj'
-const chainId =
-  '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'
 
 @suite(timeout(5000))
 class MultiWrapperTests {
@@ -40,13 +36,6 @@ class MultiWrapperTests {
       textDecoder: new TextDecoder()
     })
 
-    // Overwrite with legacy eosjs
-    this.eos = Eos({
-      httpEndpoint,
-      keyProvider: privateKey,
-      chainId
-    })
-
     this.morph = new Morpheos(this.eos)
     this.multi = new MultiWrapper(this.eos)
     this.multi.use('dgoods', DGoods)
@@ -61,27 +50,29 @@ class MultiWrapperTests {
         id: '100000000000003'
       }
     ]
-    const transaction = await this.morph.transact(
-      [
-        {
-          account: 'stanismarket',
-          name: 'sellnft',
-          authorization: [{ actor: 'account', permission: 'active' }],
-          data: {
-            owner: 'account',
-            nfts: [{ contract: items[0].contract, token_ids: [items[0].id] }],
-            price: '0.0001 EOS'
-          }
-        },
-        this.multi.transferNft(
-          'account',
-          'stanismarket',
-          items,
-          'Selling on Scatter Marketplace'
-        )
-      ],
-      { broadcast: false, sign: false }
+    const auth = { actor: 'someaccount', permission: 'active' }
+    assert.isFulfilled(
+      this.morph.transact(
+        [
+          {
+            account: 'stanismarket',
+            name: 'sellnft',
+            authorization: [auth],
+            data: {
+              owner: auth.actor,
+              nfts: [{ contract: items[0].contract, token_ids: [items[0].id] }],
+              price: '0.0001 EOS'
+            }
+          },
+          this.multi.transferNft(
+            auth,
+            'stanismarket',
+            items,
+            'Selling on Scatter Marketplace'
+          )
+        ],
+        { broadcast: false, sign: false }
+      )
     )
-    // console.log(transaction.transaction.transaction.actions)
   }
 }
