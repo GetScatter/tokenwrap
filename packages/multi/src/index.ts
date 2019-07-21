2056ae5ce93a0e75a23eae9ec3f6121345c5da3e
andresberrios <andres.berrios.j@gmail.com>
2019-07-21 21:32:42 +0200

    Accept and normalize NFT id groups in Multi

--- packages/multi/src/index.ts
+++ packages/multi/src/index.ts
@@ -4,12 +4,17 @@ import {
   Morpheos,
   SendableTransaction
 } from '@tokenwrap/core-eosio'
-import { runInNewContext } from 'vm'
 
-export interface NftIds {
-  contract: string
+export interface NftId {
   standard?: string
-  token_ids: string[]
+  contract: string
+  tokenId: string
+}
+
+export interface NftGroup {
+  standard?: string
+  contract: string
+  tokenIds: string[]
 }
 
 export interface FtAsset {
@@ -45,11 +50,11 @@ export class MultiWrapper {
   public async transferNft(
     from: FlexAuth,
     to: string,
-    ids: NftIds[],
+    ids: Array<NftGroup | NftId>,
     memo: string
   ) {
-    return this.processGroups(ids, (wrapper, groupIds) =>
-      wrapper.transferNft(from, to, groupIds, memo)
+    return this.processGroups(ids, (wrapper, tokenIds) =>
+      wrapper.transferNft(from, to, tokenIds, memo)
     )
   }
 
@@ -69,35 +74,39 @@ export class MultiWrapper {
   public async offerNft(
     owner: FlexAuth,
     newOwner: string,
-    ids: NftIds[],
+    ids: Array<NftGroup | NftId>,
     memo: string
   ) {
-    return this.processGroups(ids, (wrapper, groupIds) =>
-      wrapper.offerNft(owner, newOwner, groupIds, memo)
+    return this.processGroups(ids, (wrapper, tokenIds) =>
+      wrapper.offerNft(owner, newOwner, tokenIds, memo)
     )
   }
 
-  public async acceptNft(claimer: string, ids: NftIds[]) {
-    return this.processGroups(ids, (wrapper, groupIds) =>
-      wrapper.acceptNft(claimer, groupIds)
+  public async acceptNft(claimer: FlexAuth, ids: Array<NftGroup | NftId>) {
+    return this.processGroups(ids, (wrapper, tokenIds) =>
+      wrapper.acceptNft(claimer, tokenIds)
     )
   }
 
   public async rentOutNft(
     owner: FlexAuth,
     to: string,
-    ids: NftIds[],
+    ids: Array<NftGroup | NftId>,
     period: number | string,
     memo: string
   ) {
-    return this.processGroups(ids, (wrapper, groupIds) =>
-      wrapper.rentOutNft(owner, to, groupIds, period, memo)
+    return this.processGroups(ids, (wrapper, tokenIds) =>
+      wrapper.rentOutNft(owner, to, tokenIds, period, memo)
     )
   }
 
-  public async reclaimNft(owner: FlexAuth, from: string, ids: NftIds[]) {
-    return this.processGroups(ids, (wrapper, groupIds) =>
-      wrapper.reclaimNft(owner, from, groupIds)
+  public async reclaimNft(
+    owner: FlexAuth,
+    from: string,
+    ids: Array<NftGroup | NftId>
+  ) {
+    return this.processGroups(ids, (wrapper, tokenIds) =>
+      wrapper.reclaimNft(owner, from, tokenIds)
     )
   }
 
@@ -121,35 +130,53 @@ export class MultiWrapper {
     }
   }
 
-  private async processGroups(
-    ids: NftIds[],
-    fn: (wrapper: EosioTokenStandard, tokenIds: string[]) => SendableTransaction
-  ) {
-    for (const nftIds of ids) {
-      if (!nftIds.standard) {
-        nftIds.standard = await this.getStandard(nftIds.contract)
+  private normalizeNftGroups(ids: Array<NftGroup | NftId>) {
+    const groups: { [contract: string]: NftGroup } = {}
+    for (const item of ids) {
+      const group: NftGroup = groups[item.contract] || {
+        contract: item.contract
+      }
+      group.standard = group.standard || item.standard
+      groups[item.contract] = group
+      if ('tokenIds' in item) {
+        group.tokenIds = group.tokenIds.concat(item.tokenIds)
+      } else if ('tokenId' in item) {
+        group.tokenIds.push(item.tokenId)
+      } else {
+        throw new TypeError('Unrecognized NFT ID interface format')
       }
     }
-    const actions = Object.entries(ids).map(([contract, nfts]) => {
-      const wrapper = this.getStandardInstance(
-        nfts.standard ? nfts.standard : '',
-        nfts.contract
-      )
-      return fn(wrapper, nfts.token_ids)
-    })
+    return groups
+  }
+
+  private async processGroups(
+    ids: Array<NftGroup | NftId>,
+    fn: (wrapper: EosioTokenStandard, tokenIds: string[]) => SendableTransaction
+  ) {
+    const groups = this.normalizeNftGroups(ids)
+    const actions = await Promise.all(
+      Object.entries(groups).map(async ([contract, group]) => {
+        if (!group.standard) {
+          group.standard = await this.fetchStandard(contract)
+        }
+        const wrapper = this.getStandardInstance(group.standard, contract)
+        return fn(wrapper, group.tokenIds)
+      })
+    )
     return new SendableTransaction(actions, this.eos)
   }
 
-  private async getStandard(contract: string) {
+  private async fetchStandard(contract: string) {
     const tokenConfigs = await this.eos.getTableRow({
       code: contract,
-      scope: contract,
       table: 'tokenconfigs'
     })
-    if (tokenConfigs) {
-      return tokenConfigs.standard
+    if (!tokenConfigs || !tokenConfigs.standard) {
+      throw new Error(
+        `Could not determine NFT standard of contract '${contract}'`
+      )
     }
-    return ''
+    return tokenConfigs.standard as string
   }
 
   private ftIsSimpleassets(asset: any): asset is SimpleassetsFtAsset {
